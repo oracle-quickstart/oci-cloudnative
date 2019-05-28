dc40e18a9cd1d92ad4251f3e16fb5327d7e9f3e9
Matt Vander Vliet <matthew.vander.vliet@oracle.com>
2019-05-28 15:15:58 -0600

    Modified node api service

---
+++ src/api/api/catalogue/index.js
@@ -0,0 +1,30 @@
+(function (){
+  'use strict';
+
+  var express   = require("express")
+    , request   = require("request")
+    , endpoints = require("../endpoints")
+    , helpers   = require("../../helpers")
+    , app       = express()
+
+  app.get("/catalogue/images*", function (req, res, next) {
+    var url = endpoints.catalogueUrl + req.url.toString();
+    request.get(url)
+        .on('error', function(e) { next(e); })
+        .pipe(res);
+  });
+
+  app.get("/catalogue*", function (req, res, next) {
+    helpers.simpleHttpRequest(endpoints.catalogueUrl + req.url.toString(), res, next);
+  });
+
+  app.get("/categories", function (req, res, next) {
+    helpers.simpleHttpRequest(endpoints.categoryUrl + req.url.toString(), res, next);
+  });
+
+  app.get("/tags", function(req, res, next) {
+    helpers.simpleHttpRequest(endpoints.tagsUrl, res, next);
+  });
+
+  module.exports = app;
+}());
