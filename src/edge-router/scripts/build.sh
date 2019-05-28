58c378304c5073da01e8f158facafb7b74ac5d39
Matt Vander Vliet <matthew.vander.vliet@oracle.com>
2019-05-28 15:15:09 -0600

    Added edge-router with ingress rules for api + storefront

---
+++ src/edge-router/scripts/build.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -ev
+
+SCRIPT_DIR=$(dirname "$0")
+NAMESPACE="mushop"
+
+TAG=$1
+if [[ -n "$CI" ]]; then
+  TAG=$WERCKER_GIT_COMMIT
+fi
+
+if [[ -z "$TAG" ]] ; then
+  echo "Cannot determine TAG"
+  exit 1
+fi
+
+if [[ "$(uname)" == "Darwin" ]]; then
+  DOCKER_CMD=docker
+else
+  DOCKER_CMD="sudo docker"
+fi
+
+REPO=${NAMESPACE}/$(basename edge-router);
+
+echo "Building... $REPO:$TAG"
+$DOCKER_CMD build -t ${REPO}:${TAG} .
