fe839f174961042e64b545e95e149ca04c2572f0
Peter Jausovec <peter.jausovec@gmail.com>
2019-07-25 16:11:55 -0700

    Updating payment service to gomod

--- src/payment/transport.go
+++ src/payment/transport.go
@@ -7,17 +7,18 @@ import (
 	"io/ioutil"
 	"net/http"
 
-	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/circuitbreaker"
+	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/tracing/opentracing"
 	httptransport "github.com/go-kit/kit/transport/http"
 	"github.com/gorilla/mux"
 	stdopentracing "github.com/opentracing/opentracing-go"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-        "github.com/streadway/handy/breaker"
+	"github.com/streadway/handy/breaker"
 	"golang.org/x/net/context"
 )
 
+//hello
 // MakeHTTPHandler mounts the endpoints into a REST-y HTTP handler.
 func MakeHTTPHandler(ctx context.Context, e Endpoints, logger log.Logger, tracer stdopentracing.Tracer) *mux.Router {
 	r := mux.NewRouter().StrictSlash(false)
@@ -27,18 +28,16 @@ func MakeHTTPHandler(ctx context.Context, e Endpoints, logger log.Logger, tracer
 	}
 
 	r.Methods("POST").Path("/paymentAuth").Handler(httptransport.NewServer(
-		ctx,
 		circuitbreaker.HandyBreaker(breaker.NewBreaker(0.2))(e.AuthoriseEndpoint),
 		decodeAuthoriseRequest,
 		encodeAuthoriseResponse,
-		append(options, httptransport.ServerBefore(opentracing.FromHTTPRequest(tracer, "POST /paymentAuth", logger)))...,
+		append(options, httptransport.ServerBefore(opentracing.ContextToHTTP(tracer, logger)))...,
 	))
 	r.Methods("GET").Path("/health").Handler(httptransport.NewServer(
-		ctx,
 		circuitbreaker.HandyBreaker(breaker.NewBreaker(0.2))(e.HealthEndpoint),
 		decodeHealthRequest,
 		encodeHealthResponse,
-		append(options, httptransport.ServerBefore(opentracing.FromHTTPRequest(tracer, "GET /health", logger)))...,
+		append(options, httptransport.ServerBefore(opentracing.ContextToHTTP(tracer, logger)))...,
 	))
 	r.Handle("/metrics", promhttp.Handler())
 	return r
