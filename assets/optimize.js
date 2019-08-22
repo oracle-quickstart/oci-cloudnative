3e1833e5c612befae5de842ce004c83948c7b467
Matt Vander Vliet <matthew.vander.vliet@oracle.com>
2019-08-22 13:50:20 -0600

    Script for uploading/optimizing images for Object Storage

---
+++ assets/optimize.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const imagemin = require('imagemin');
+const pngquant = require('imagemin-pngquant');
+const jpegtran = require('imagemin-jpegtran');
+const mozjpeg = require('imagemin-mozjpeg');
+
+const config = require('./config.json');
+const dist = path.join(__dirname, config.dist);
+
+
+const optimize = async dir => await imagemin([`${dir}/*`], {
+  destination: dist,
+  plugins: [
+    pngquant(),
+    jpegtran({progressive: true}),
+    mozjpeg({quality: 85}),
+  ],
+}).then(files => console.log(`Optimized ${files.length} images in ./${dir}`));
+
+Promise.all(['hero', 'products'].map(optimize))
+  .catch(e => {
+    console.error(e);
+    process.exit(1);
+  });
