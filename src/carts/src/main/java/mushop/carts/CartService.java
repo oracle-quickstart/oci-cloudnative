a55dc56a40b1ac1a637a3e652e9d23eba88c9762
Jeevan Joseph <jeevan.g@gmail.com>
2020-01-13 13:57:13 -0800

    Added logging to the carts service and fallback to MockMode when DB is not available.

--- src/carts/src/main/java/mushop/carts/CartService.java
+++ src/carts/src/main/java/mushop/carts/CartService.java
@@ -4,6 +4,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Set;
+import java.util.logging.Logger;
 
 import io.helidon.common.http.Http;
 import io.helidon.common.http.Http.RequestMethod;
@@ -14,38 +15,48 @@ import io.helidon.webserver.ServerResponse;
 import io.helidon.webserver.Service;
 
 public class CartService implements Service {
-    
-    /** A reserved dbname to use for an in-memory repository instead of the autonomous database */
+
+    /**
+     * A reserved dbname to use for an in-memory repository instead of the
+     * autonomous database
+     */
     private static final String MOCKDB = "mock";
-    
+
+    private final static Logger log = Logger.getLogger(CartService.class.getName());
+
     /** @see https://github.com/oracle/helidon/issues/1172 */
-    private static final Set<RequestMethod> PATCH = 
-       Collections.singleton(Http.RequestMethod.create("PATCH"));
-    
-    private final CartRepository carts;
-    
+    private static final Set<RequestMethod> PATCH = Collections.singleton(Http.RequestMethod.create("PATCH"));
+
+    private CartRepository carts;
+
     public CartService(Config config) {
         String dbName = config.get("OADB_SERVICE").asString().get();
+        log.info("Connecting to "+dbName);
         if (MOCKDB.equalsIgnoreCase(dbName)) {
             carts = new CartRepositoryMemoryImpl();
         } else {
-            carts = new CartRepositoryDatabaseImpl(config);
+            try {
+                carts = new CartRepositoryDatabaseImpl(config);
+                log.info("Connected to "+dbName);
+            }catch(RuntimeException ex){
+                carts = new CartRepositoryMemoryImpl();
+                log.warning("Connecting to "+dbName+" failed. Falling back to mock DB.");
+            }
         }
     }
-    
+
     @Override
     public void update(Rules rules) {
         rules.get("/{cartId}/items", this::getCartItems)
-             .delete("/{cartId}", this::deleteCart)
-             .delete("/{cartId}/items/{itemId}", this::deleteCartItem)
-             .post("/{cartId}", this::postCart)
-             .anyOf(PATCH, "/{cartId}/items", this::updateCartItem);
+            .delete("/{cartId}", this::deleteCart)
+            .delete("/{cartId}/items/{itemId}", this::deleteCartItem).post("/{cartId}", this::postCart)
+            .anyOf(PATCH, "/{cartId}/items", this::updateCartItem);
     }
 
-    /** 
+    /**
      * GET /{cartId}/items
      * 
-     * Returns the list of items in a cart. 
+     * Returns the list of items in a cart.
      */
     public void getCartItems(ServerRequest request, ServerResponse response) {
         Cart result = null;
@@ -59,14 +70,14 @@ public class CartService implements Service {
         } catch (Exception e) {
             sendError(response, e.getMessage());
             return;
-        } 
+        }
         response.status(200).send(result.getItems());
     }
 
-    /** 
+    /**
      * DELETE /{cartId}
      * 
-     * Delete a cart. 
+     * Delete a cart.
      */
     public void deleteCart(ServerRequest request, ServerResponse response) {
         try {
@@ -78,16 +89,16 @@ public class CartService implements Service {
         } catch (Exception e) {
             sendError(response, e.getMessage());
             return;
-        } 
+        }
     }
-    
+
     /**
      * DELETE /{cartId}/items/{itemId}
      * 
      * Deletes item in a cart.
      */
-    public void deleteCartItem(ServerRequest request, ServerResponse response) { 
-        
+    public void deleteCartItem(ServerRequest request, ServerResponse response) {
+
         String cartId = request.path().param("cartId");
         String itemId = request.path().param("itemId");
         try {
@@ -104,22 +115,22 @@ public class CartService implements Service {
             return;
         }
     }
-    
+
     /**
      * POST /{cartId}
      * 
-     * Adds or updates a cart.  If the cartId does not exist, a new cart is added.  
-     * If the cartId does exist, the given cart is merged in to the existing one.
-     * This method is used to add additional items to a cart. 
+     * Adds or updates a cart. If the cartId does not exist, a new cart is added. If
+     * the cartId does exist, the given cart is merged in to the existing one. This
+     * method is used to add additional items to a cart.
      */
     public void postCart(ServerRequest request, ServerResponse response) {
         String cartId = request.path().param("cartId");
-        
+
         try {
             request.content().as(Cart.class).thenAccept(newCart -> {
                 Cart cart = carts.getById(cartId);
                 if (cart == null) {
-                    
+
                     newCart.setId(cartId);
                     carts.save(newCart);
                     response.status(201).send(); // created
@@ -134,11 +145,11 @@ public class CartService implements Service {
             return;
         }
     }
-    
+
     /**
      * PUT /{cartId}/items
      * 
-     * Updates the quantity of an item in the cart. 
+     * Updates the quantity of an item in the cart.
      */
     public void updateCartItem(ServerRequest request, ServerResponse response) {
         String cartId = request.path().param("cartId");
@@ -163,10 +174,11 @@ public class CartService implements Service {
             return;
         }
     }
-    
+
     private void sendError(ServerResponse response, String message) {
         HashMap<String, String> error = new HashMap<String, String>();
         error.put("errorMessage", message);
         response.status(400).send(error);
     }
+    
 }
