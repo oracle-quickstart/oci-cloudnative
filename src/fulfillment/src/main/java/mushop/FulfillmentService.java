0daafda283acea6447081d53514993436e04ffec
Jeevan Joseph <jeevan.g@gmail.com>
2020-01-24 12:47:06 -0800

    Moving NATS config to application.yaml

--- src/fulfillment/src/main/java/mushop/FulfillmentService.java
+++ src/fulfillment/src/main/java/mushop/FulfillmentService.java
@@ -2,6 +2,7 @@ package mushop;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.micronaut.context.annotation.Value;
 import io.micronaut.scheduling.annotation.Scheduled;
 import io.nats.client.Connection;
 import io.nats.client.Dispatcher;
@@ -17,13 +18,18 @@ import java.nio.charset.StandardCharsets;
 @Singleton
 public class FulfillmentService {
     private Connection nc;
-    private final String MUSHOP_ORDERS_SUBJECT = "mushop-orders";
     private final ObjectMapper objectMapper = new ObjectMapper();
     private static final Logger LOG = LoggerFactory.getLogger(FulfillmentService.class);
+    private String NATS_URL;
+    private String MUSHOP_ORDERS_SUBJECT;
 
-    public FulfillmentService() {
+    public FulfillmentService(@Value("nats://${mushop.orderupdates.host}:${mushop.orderupdates.port}") String NATS_URL,
+                              @Value("${mushop.orderupdates.subject}") String MUSHOP_ORDERS_SUBJECT) {
+        this.MUSHOP_ORDERS_SUBJECT = MUSHOP_ORDERS_SUBJECT;
+        this.NATS_URL = NATS_URL;
         try {
-            nc = Nats.connect("nats://localhost:4222");
+            LOG.info("Connecting to NATS {} and subscribing to subject {}", NATS_URL, MUSHOP_ORDERS_SUBJECT);
+            nc = Nats.connect(this.NATS_URL);
             Dispatcher d = nc.createDispatcher((msg) -> {
                 try {
                     handleMessage(msg);
@@ -31,7 +37,7 @@ public class FulfillmentService {
                     e.printStackTrace();
                 }
             });
-            d.subscribe(MUSHOP_ORDERS_SUBJECT);
+            d.subscribe(this.MUSHOP_ORDERS_SUBJECT);
         } catch (IOException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
@@ -39,13 +45,13 @@ public class FulfillmentService {
         }
     }
 
+
     @Scheduled(fixedDelay = "10s")
     public void publish() throws JsonProcessingException {
         OrderUpdate update = new OrderUpdate("ORD-123", new Shipment("SHP-123", "Processing"));
         String msg = objectMapper.writeValueAsString(update);
         LOG.info("Sending update {}", msg);
         nc.publish(MUSHOP_ORDERS_SUBJECT, msg.getBytes(StandardCharsets.UTF_8));
-
     }
 
     private void handleMessage(Message message) throws IOException {
