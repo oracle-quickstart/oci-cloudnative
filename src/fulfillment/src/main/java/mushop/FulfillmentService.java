b88da5264b8dfc82fab272e96b04077fd0b96114
Jeevan Joseph <jeevan.g@gmail.com>
2020-01-29 15:50:33 -0800

    Split subjects orders and updates. Added retries for NATs connections.

--- src/fulfillment/src/main/java/mushop/FulfillmentService.java
+++ src/fulfillment/src/main/java/mushop/FulfillmentService.java
@@ -18,7 +18,10 @@ import javax.inject.Singleton;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.UUID;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 @Singleton
 @Introspected
@@ -26,33 +29,49 @@ public class FulfillmentService {
     private Connection nc;
     private final ObjectMapper objectMapper = new ObjectMapper();
     private static final Logger LOG = LoggerFactory.getLogger(FulfillmentService.class);
-    @Value("nats://${mushop.orderupdates.host}:${mushop.orderupdates.port}")
+    @Value("nats://${mushop.messaging.host}:${mushop.messaging.port}")
     private String NATS_URL;
-    @Value("${mushop.orderupdates.subject}")
+    @Value("${mushop.messaging.subjects.orders}")
     private String MUSHOP_ORDERS_SUBJECT;
+    @Value("${mushop.messaging.subjects.shipments}")
+    private String MUSHOP_SHIPMENTS_SUBJECT;
 
     public FulfillmentService() {
     }
 
     @EventListener
     @Async
-    public void connect(final ServiceStartedEvent event){
-        try {
-            LOG.info("Connecting to NATS {} and subscribing to subject {}", this.NATS_URL, this.MUSHOP_ORDERS_SUBJECT);
-            nc = Nats.connect(this.NATS_URL);
-            Dispatcher d = nc.createDispatcher((msg) -> {
-                try {
-                    handleMessage(msg);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            });
-            d.subscribe(this.MUSHOP_ORDERS_SUBJECT);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+    public void connect(final ServiceStartedEvent event) throws InterruptedException {
+        LOG.info("Connecting to NATS {} and subscribing to subject {}", this.NATS_URL, this.MUSHOP_ORDERS_SUBJECT);
+        boolean connected = false;
+        while (!connected) {
+            try {
+                Future<Boolean> result = Executors.newSingleThreadExecutor().submit(() -> {
+                    try {
+                        nc = Nats.connect(this.NATS_URL);
+                        Dispatcher d = nc.createDispatcher((msg) -> {
+                            try {
+                                handleMessage(msg);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        });
+                        d.subscribe(this.MUSHOP_ORDERS_SUBJECT);
+                        return Boolean.TRUE;
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        LOG.error("Connection failed due to {}. Retrying in 5s", e.getMessage());
+                        Thread.sleep(5000l);
+                        return Boolean.FALSE;
+                    }
+                });
+                connected = result.get();
+            }  catch (ExecutionException e) {
+                e.printStackTrace();
+            }
         }
+        LOG.info("Connected to NATS {} and subscribed to subject {}", this.NATS_URL, this.MUSHOP_ORDERS_SUBJECT);
+
     }
 
     private void handleMessage(Message message) throws IOException {
@@ -69,7 +88,7 @@ public class FulfillmentService {
                 Shipment shipment = new Shipment(UUID.randomUUID().toString(), "Shipped");
                 order.setShipment(shipment);
                 String msg = objectMapper.writeValueAsString(order);
-                nc.publish(MUSHOP_ORDERS_SUBJECT, msg.getBytes(StandardCharsets.UTF_8));
+                nc.publish(MUSHOP_SHIPMENTS_SUBJECT, msg.getBytes(StandardCharsets.UTF_8));
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } catch (JsonProcessingException e) {
