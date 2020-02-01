f19bbe4fafdd98f3a21d8e4cf2695145479814a6
Jeevan Joseph <jeevan.g@gmail.com>
2020-01-31 23:42:29 -0800

    Updated docs

--- src/fulfillment/docker-build.sh
+++ src/fulfillment/docker-build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-docker build . -t fulfillment
+docker build . -t mushop-fulfillment:latest
 echo
 echo
 echo "To run the docker container execute:"
-echo "    $ docker run -p 8080:8080 fulfillment"
+echo "    $ docker run -p 8099:8099 mushop-fulfillment:latest"
