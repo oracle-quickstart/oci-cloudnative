e981848a8a89a6aeb2dd003c83821a91f20530fd
Adao Junior <junior@users.noreply.github.com>
2020-02-06 09:51:58 -0600

    included header

--- src/catalogue/service_test.go
+++ src/catalogue/service_test.go
@@ -1,3 +1,7 @@
+/*
+** Copyright © 2020, Oracle and/or its affiliates. All rights reserved.
+** Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
+ */
 package catalogue
 
 import (
