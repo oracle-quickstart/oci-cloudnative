6f6446272e96303d8f5b7cd4865bb4bf38c2eb93
Peter Jausovec <peter.jausovec@gmail.com>
2020-02-14 10:44:01 -0800

    Add tracing middleware that injects tracing headers into each outgoing request.

--- src/api/app.js
+++ src/api/app.js
@@ -2,34 +2,33 @@
  * Copyright © 2019, Oracle and/or its affiliates. All rights reserved.
  * The Universal Permissive License (UPL), Version 1.0
  */
-var express = require("express")
-  , morgan = require("morgan")
-  , bodyParser = require("body-parser")
-  , cookieParser = require("cookie-parser")
-  , session = require("express-session")
-  , Config = require("./config")
-  , helpers = require("./helpers")
-  , mock = require("./api/mock")
-  , cart = require("./api/cart")
-  , catalogue = require("./api/catalogue")
-  , config = require("./api/config")
-  , orders = require("./api/orders")
-  , user = require("./api/user")
-  , metrics = require("./api/metrics")
-  , health = require("./api/health")
-  , newsletter = require("./api/newsletter")
-  , app = express();
+var express = require('express'),
+    morgan = require('morgan'),
+    bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'),
+    session = require('express-session'),
+    Config = require('./config'),
+    helpers = require('./helpers'),
+    mock = require('./api/mock'),
+    cart = require('./api/cart'),
+    catalogue = require('./api/catalogue'),
+    config = require('./api/config'),
+    orders = require('./api/orders'),
+    user = require('./api/user'),
+    metrics = require('./api/metrics'),
+    health = require('./api/health'),
+    newsletter = require('./api/newsletter'),
+    app = express();
 
 app.use(helpers.rewriteSlash);
+app.use(helpers.tracingMiddleware);
 app.use(metrics);
 app.use(health);
 app.use(session(Config.session()));
 
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(morgan(Config.prod() ? "combined" : "dev", {
-  
-}));
+app.use(morgan(Config.prod() ? 'combined' : 'dev', {}));
 
 /* Mount API endpoints */
 const api = express.Router();
