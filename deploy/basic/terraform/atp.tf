4837082456a12170cc040bcc6d1c3bc8c2d6148e
Adao Junior <junior@users.noreply.github.com>
2020-06-18 15:57:19 -0500

    removed optional extra parameters

--- deploy/basic/terraform/atp.tf
+++ deploy/basic/terraform/atp.tf
@@ -4,21 +4,13 @@
 
 # creates an ATP database
 resource "oci_database_autonomous_database" "mushop_autonomous_database" {
-  #Required
-  admin_password           = random_string.autonomous_database_wallet_password.result
-  compartment_id           = var.compartment_ocid
-  cpu_core_count           = 1
-  data_storage_size_in_tbs = 1
-  db_name                  = var.database_name
-  freeform_tags            = local.common_tags
-  is_free_tier             = true
-
-  #Optional
-
-  db_workload                                    = "OLTP"
+  admin_password                                 = random_string.autonomous_database_wallet_password.result
+  compartment_id                                 = var.compartment_ocid
+  cpu_core_count                                 = 1
+  data_storage_size_in_tbs                       = 1
+  db_name                                        = var.database_name
+  freeform_tags                                  = local.common_tags
+  is_free_tier                                   = true
   display_name                                   = "${var.database_name}${random_id.mushop_id.dec}"
-  is_auto_scaling_enabled                        = false
-  is_dedicated                                   = false
   is_preview_version_with_service_terms_accepted = false
-
 }
