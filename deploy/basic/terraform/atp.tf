ea405f9296da376bdcdb25d08731671b7e09c127
Adao Junior <junior@users.noreply.github.com>
2020-07-09 17:04:14 -0500

    updated the variables
    Stop using hardcoded values on the main scripts.
    
    Includes workaround to avoid issues on some regions

--- deploy/basic/terraform/atp.tf
+++ deploy/basic/terraform/atp.tf
@@ -9,9 +9,9 @@ resource "oci_database_autonomous_database" "mushop_autonomous_database" {
   cpu_core_count                                 = 1
   data_storage_size_in_tbs                       = 1
   db_name                                        = var.database_name
+  db_version                                     = var.autonomous_database_db_version
   display_name                                   = "${var.database_name}${random_id.mushop_id.dec}"
   freeform_tags                                  = local.common_tags
-  is_free_tier                                   = true
-  license_model                                  = "LICENSE_INCLUDED"
-  is_preview_version_with_service_terms_accepted = false
+  is_free_tier                                   = var.autonomous_database_is_free_tier
+  license_model                                  = var.autonomous_database_license_model
 }
