080759b935f6bcaace15877601542351466b028b
Adao Junior <junior@users.noreply.github.com>
2020-07-09 20:25:18 -0500

    get image without need to specify region

--- deploy/basic/terraform/datasources.tf
+++ deploy/basic/terraform/datasources.tf
@@ -49,3 +49,13 @@ data "oci_limits_limit_values" "test_limit_values" {
   name                = "vm-standard-e2-1-micro-count"
   scope_type          = "AD"
 }
+
+# Gets a list of supported images based on the shape, operating_system and operating_system_version provided
+data "oci_core_images" "compute_images" {
+  compartment_id           = var.compartment_ocid
+  operating_system         = var.image_operating_system
+  operating_system_version = var.image_operating_system_version
+  shape                    = var.instance_shape
+  sort_by                  = "TIMECREATED"
+  sort_order               = "DESC"
+}
