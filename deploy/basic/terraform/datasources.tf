9a53908cce88a5c7c53d594f23ccb08ffacc7fed
Adao Junior <junior@users.noreply.github.com>
2020-07-09 20:12:16 -0500

    copyright date updated

--- deploy/basic/terraform/datasources.tf
+++ deploy/basic/terraform/datasources.tf
@@ -1,24 +1,17 @@
-# Copyright (c) 2019 Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2019, 2020 Oracle and/or its affiliates. All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
 # 
 
 # Gets a list of Availability Domains
 data "oci_identity_availability_domains" "ADs" {
-
   compartment_id = var.tenancy_ocid
-
 }
-
 
 data "oci_objectstorage_namespace" "user_namespace" {
-
   compartment_id = var.compartment_ocid
-
 }
 
-
 resource "random_string" "autonomous_database_wallet_password" {
-
   length           = 16
   special          = true
   min_upper        = 3
@@ -26,7 +19,6 @@ resource "random_string" "autonomous_database_wallet_password" {
   min_numeric      = 3
   min_special      = 3
   override_special = "{}#^*<>[]%~"
-
 }
 
 resource "random_id" "mushop_id" {
@@ -34,15 +26,12 @@ resource "random_id" "mushop_id" {
 }
 
 data "oci_database_autonomous_database_wallet" "autonomous_database_wallet" {
-
   autonomous_database_id = oci_database_autonomous_database.mushop_autonomous_database.id
   password               = random_string.autonomous_database_wallet_password.result
   base64_encode_content  = "true"
-
 }
 
 data "oci_limits_services" "test_services" {
-  #Required
   compartment_id = var.tenancy_ocid
 
   filter {
