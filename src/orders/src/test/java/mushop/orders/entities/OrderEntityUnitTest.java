859fde641b3bc0fb1f61ba9193785374db2afe09
Jeevan Joseph <jeevan.g@gmail.com>
2020-07-16 19:43:27 -0700

    Updated tests

--- src/orders/src/test/java/mushop/orders/entities/OrderEntityUnitTest.java
+++ src/orders/src/test/java/mushop/orders/entities/OrderEntityUnitTest.java
@@ -16,16 +16,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DataJpaTest
 public class OrderEntityUnitTest {
 
-    public TestEntityManager getEntityManager() {
-        return entityManager;
-    }
-
-    public void setEntityManager(TestEntityManager entityManager) {
-        this.entityManager = entityManager;
-    }
-
     @Autowired
     private TestEntityManager entityManager;
+
+    @Autowired
+    private CustomerOrderRepository orderRepository;
 
     public CustomerOrderRepository getOrderRepository() {
         return orderRepository;
@@ -35,12 +30,15 @@ public class OrderEntityUnitTest {
         this.orderRepository = orderRepository;
     }
 
-    @Autowired
-    private CustomerOrderRepository orderRepository;
+    public TestEntityManager getEntityManager() {
+        return entityManager;
+    }
 
-    @Test
-    public void whenFindByName_thenReturnEmployee() {
-        // given
+    public void setEntityManager(TestEntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public CustomerOrder createOrderObj(){
         Address address = new Address("id","000","street","city","00000","coutry");
         Card card = new Card("id","0000000000000000","00/00","000");
         Customer customer = new Customer("cust001","first","last","user", Collections.emptyList(),Collections.emptyList());
@@ -52,13 +50,45 @@ public class OrderEntityUnitTest {
                 null,
                 new Date(),
                 0.0f);
+        return order;
+    }
+
+    @Test
+    public void findOrdersByCustomerId() {
+        CustomerOrder order = createOrderObj();
         entityManager.persist(order);
         entityManager.flush();
-
-        // when
+        Long orderId = order.getId();
         Page<CustomerOrder> found =  orderRepository.findByCustomerId("cust001", Pageable.unpaged());
-        found.forEach(orderFound -> assertThat(orderFound.getId()).isEqualTo(order.getId()) );
+        found.forEach(orderFound -> assertThat(orderFound.getId()).isEqualTo(orderId) );
 
+    }
+
+    @Test
+    public void findOrderByInvalidIdFails(){
+        CustomerOrder order = createOrderObj();
+        entityManager.persist(order);
+        entityManager.flush();
+        Long orderId = order.getId();
+        assertThat(orderRepository.findById(orderId+50).isPresent()).isFalse();
+    }
+
+    @Test
+    public void findOrderByIdSucceeds(){
+        CustomerOrder order = createOrderObj();
+        entityManager.persist(order);
+        entityManager.flush();
+        Long orderId = order.getId();
+        assertThat(orderRepository.findById(orderId).isPresent()).isTrue();
+    }
+
+    @Test
+    public void addressIsPersisted(){
+        CustomerOrder order = createOrderObj();
+        entityManager.persist(order);
+        entityManager.flush();
+        Long orderId = order.getId();
+        assertThat(orderRepository.findById(orderId).get().getAddress()).isEqualTo(order.getAddress());
     }
 
 }
