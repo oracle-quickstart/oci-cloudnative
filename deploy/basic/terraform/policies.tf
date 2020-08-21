17a73a0fc559480f33d140f4f1a24529d87d4e7e
Adao Junior <junior@users.noreply.github.com>
2020-08-21 15:20:08 -0500

    optional policy for vault

--- deploy/basic/terraform/policies.tf
+++ deploy/basic/terraform/policies.tf
@@ -10,3 +10,17 @@ resource "oci_identity_policy" "mushop_allow_object_storage_lifecycle" {
   statements     = ["Allow service objectstorage-${var.region} to manage object-family in compartment id ${var.compartment_ocid}"]
   freeform_tags  = local.common_tags
 }
+
+# Create pilicy to allow use OCI Vault/KMS
+resource "oci_identity_policy" "mushop_allow_manage_vaults_and_keys" {
+  name           = "mushop-vault-and-keys-${random_id.mushop_id.dec}"
+  description    = "policy created by terraform for MuShop Basic"
+  compartment_id = var.compartment_ocid
+  statements     = [
+      "Allow group ${var.user_admin_group} to manage vaults in compartment id ${var.compartment_ocid}",
+      "Allow group ${var.user_admin_group} to manage keys in compartment id ${var.compartment_ocid}"
+      ]
+  freeform_tags  = local.common_tags
+
+   count = var.create_vault_policies ? 1 : 0
+}
