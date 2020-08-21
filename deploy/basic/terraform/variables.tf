6b7c511c663d576d3f87b09f26aca51c34afc91a
Adao Junior <junior@users.noreply.github.com>
2020-08-21 15:20:20 -0500

    encryption variables

--- deploy/basic/terraform/variables.tf
+++ deploy/basic/terraform/variables.tf
@@ -44,3 +44,40 @@ variable "autonomous_database_license_model" {
 variable "autonomous_database_is_free_tier" {
   default = true
 }
+
+# Encryption (OCI Vault/Key Management/KMS)
+
+variable "is_pv_encryption_in_transit_enabled" {
+  default = false
+}
+variable "use_encryption_from_oci_vault" {
+  default     = false
+}
+variable "create_new_encryption_key" {
+  default = false
+}
+variable "encryption_key_id" {
+  default = ""
+}
+variable "create_vault_policies" {
+  default = false
+}
+variable "user_admin_group" {
+  default = "Administrators"
+}
+variable "vault_display_name" {
+  default = "OciTester Vault"
+}
+variable "vault_type" {
+  type    = list
+  default = ["DEFAULT", "VIRTUAL_PRIVATE"]
+}
+variable "vault_key_display_name" {
+  default = "OciTester Key"
+}
+variable "vault_key_key_shape_algorithm" {
+  default = "AES"
+}
+variable "vault_key_key_shape_length" {
+  default = 32
+}
