a351c42dacd3992aef01c6e4666d9e8cbcaad22a
Adao Junior <junior@users.noreply.github.com>
2020-08-21 16:49:50 -0500

    proper variables for atp to support more cpus

--- deploy/basic/terraform/atp.tf
+++ deploy/basic/terraform/atp.tf
@@ -6,8 +6,8 @@
 resource "oci_database_autonomous_database" "mushop_autonomous_database" {
   admin_password           = random_string.autonomous_database_admin_password.result
   compartment_id           = var.compartment_ocid
-  cpu_core_count           = 1
-  data_storage_size_in_tbs = 1
+  cpu_core_count           = var.autonomous_database_cpu_core_count
+  data_storage_size_in_tbs = var.autonomous_database_data_storage_size_in_tbs
   db_name                  = "${var.autonomous_database_name}${random_string.deploy_id.result}"
   db_version               = var.autonomous_database_db_version
   display_name             = "${var.autonomous_database_name}-${random_string.deploy_id.result}"
