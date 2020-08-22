be0099245a708f51126f3f9d80c74f1034ddb4b5
Adao Junior <junior@users.noreply.github.com>
2020-08-22 01:53:38 -0500

    include reference providers and cleanup

--- deploy/basic/terraform/provider.tf
+++ deploy/basic/terraform/provider.tf
@@ -4,12 +4,13 @@
 
 terraform {
   required_version = ">= 0.12.29"
+  # required_providers {
+  #   oci = ">= 3.90"
+  #   tls = ">= 2.0"
+  #   random = ">= 2.1"
+  # }
 }
 
 provider "oci" {
   tenancy_ocid = var.tenancy_ocid
-  # user_ocid        = "${var.user_ocid}"
-  # fingerprint      = "${var.fingerprint}"
-  # private_key_path = "${var.private_key_path}"
-  # region           = "${var.region}"
 }
