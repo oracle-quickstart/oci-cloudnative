ca3808f596e22fed76a9b4a352196a546fd236f5
Adao Junior <junior@users.noreply.github.com>
2020-08-22 01:44:33 -0500

    bump tf minimum requirement
    Compatible to ORM

--- deploy/basic/terraform/provider.tf
+++ deploy/basic/terraform/provider.tf
@@ -1,9 +1,9 @@
-# Copyright (c) 2019 Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2019, 2020 Oracle and/or its affiliates. All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
 # 
 
 terraform {
-  required_version = ">= 0.12.16"
+  required_version = ">= 0.12.29"
 }
 
 provider "oci" {
