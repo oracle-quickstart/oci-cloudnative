998e34fb509ad7b0d2cc80964bcdf2e583574ec8
Adao Junior <junior@users.noreply.github.com>
2020-08-25 09:30:58 -0500

    sl for visibility

--- deploy/basic/terraform/security-lists.tf
+++ deploy/basic/terraform/security-lists.tf
@@ -33,3 +33,35 @@ resource "oci_core_security_list" "mushop_security_list" {
     }
   }
 }
+
+resource "oci_core_security_list" "mushop_lb_security_list" {
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_virtual_network.mushop_main_vcn.id
+  display_name   = "mushop-lb-${random_string.deploy_id.result}"
+  freeform_tags  = local.common_tags
+
+  egress_security_rules {
+    protocol    = "6"
+    destination = "0.0.0.0/0"
+  }
+
+  ingress_security_rules {
+    protocol = "6"
+    source   = "0.0.0.0/0"
+
+    tcp_options {
+      max = "80"
+      min = "80"
+    }
+  }
+
+  ingress_security_rules {
+    protocol = "6"
+    source   = "0.0.0.0/0"
+
+    tcp_options {
+      max = "22"
+      min = "22"
+    }
+  }
+}
