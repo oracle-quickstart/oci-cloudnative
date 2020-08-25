b80262705a064a8a13efd6a0929eb973d30619e2
Adao Junior <junior@users.noreply.github.com>
2020-08-25 09:30:47 -0500

    visibility feature

--- deploy/basic/terraform/network.tf
+++ deploy/basic/terraform/network.tf
@@ -28,7 +28,7 @@ resource "oci_core_subnet" "mushop_lb_subnet" {
   display_name               = "mushop-lb-${random_string.deploy_id.result}"
   dns_label                  = "mushoplb${random_string.deploy_id.result}"
   security_list_ids          = [oci_core_security_list.mushop_lb_security_list.id]
-  compartment_id             = var.compartment_ocid
+  compartment_id             = (var.lb_compartment_ocid != "") ? var.lb_compartment_ocid : var.compartment_ocid
   vcn_id                     = oci_core_virtual_network.mushop_main_vcn.id
   route_table_id             = oci_core_route_table.mushop_lb_route_table.id
   dhcp_options_id            = oci_core_virtual_network.mushop_main_vcn.default_dhcp_options_id
@@ -45,12 +45,12 @@ resource "oci_core_route_table" "mushop_main_route_table" {
   route_rules {
     destination       = lookup(var.network_cidrs, "ALL-CIDR")
     destination_type  = "CIDR_BLOCK"
-    network_entity_id = (var.cluster_visibility == "Private") ? oci_core_nat_gateway.oke_mushop_nat_gateway[0].id : oci_core_internet_gateway.oke_mushop_internet_gateway.id
+    network_entity_id = (var.instance_visibility == "Private") ? oci_core_nat_gateway.mushop_nat_gateway[0].id : oci_core_internet_gateway.mushop_internet_gateway.id
   }
 }
 
 resource "oci_core_route_table" "mushop_lb_route_table" {
-  compartment_id = var.compartment_ocid
+  compartment_id = (var.lb_compartment_ocid != "") ? var.lb_compartment_ocid : var.compartment_ocid
   vcn_id         = oci_core_virtual_network.mushop_main_vcn.id
   display_name   = "mushop-lb-${random_string.deploy_id.result}"
   freeform_tags  = local.common_tags
@@ -58,7 +58,7 @@ resource "oci_core_route_table" "mushop_lb_route_table" {
   route_rules {
     destination       = lookup(var.network_cidrs, "ALL-CIDR")
     destination_type  = "CIDR_BLOCK"
-    network_entity_id = oci_core_internet_gateway.oke_mushop_internet_gateway.id
+    network_entity_id = oci_core_internet_gateway.mushop_internet_gateway.id
   }
 }
 
@@ -73,7 +73,7 @@ resource "oci_core_nat_gateway" "mushop_nat_gateway" {
 }
 
 resource "oci_core_internet_gateway" "mushop_internet_gateway" {
-  compartment_id = var.compartment_ocid
+  compartment_id = (var.lb_compartment_ocid != "") ? var.lb_compartment_ocid : var.compartment_ocid
   display_name   = "mushop-internet-gateway-${random_string.deploy_id.result}"
   vcn_id         = oci_core_virtual_network.mushop_main_vcn.id
   freeform_tags  = local.common_tags
