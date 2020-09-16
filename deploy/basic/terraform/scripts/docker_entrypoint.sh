bfb8f876994c2c1b99619df75bbb338ba1dbdaa5
Adao Junior <junior@users.noreply.github.com>
2020-09-15 23:59:58 -0500

    docker entrypoint chage

--- deploy/basic/terraform/scripts/docker_entrypoint.sh
+++ deploy/basic/terraform/scripts/docker_entrypoint.sh
@@ -8,15 +8,11 @@
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
 # 
 
-# To mock all services, change to MOCK_MODE=all
-export MOCK_MODE=${mock_mode}
+# To mock all services, change to MOCK_MODE=all or for each service MOCK_MODE=carts,orders,users
+export MOCK_MODE=all
 export NODE_ENV=production
-export CATALOGUE_PORT=${catalogue_port}
-export CATALOGUE_URL=http://localhost:${catalogue_port}
-export OADB_USER=catalogue_user
-export OADB_PW='${catalogue_password}'
-export OADB_SERVICE=${db_name}_tp
-export STATIC_MEDIA_URL=${assets_url}
+export CATALOGUE_PORT=3005
+export CATALOGUE_URL=http://localhost:${CATALOGUE_PORT}
 
 echo "Environment: $(uname -a)";
 
