d6e7e822525c941706ccdf209dd30990b4b4b717
Adao Junior <junior@users.noreply.github.com>
2021-01-14 18:49:48 -0600

    renaming providers and removed deprecated entry

--- deploy/complete/terraform/providers.tf
+++ deploy/complete/terraform/providers.tf
@@ -1,4 +1,4 @@
-# Copyright (c) 2020 Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2020, 2021 Oracle and/or its affiliates. All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
 # 
 
@@ -28,7 +28,6 @@ provider "kubernetes" {
 # https://docs.cloud.oracle.com/en-us/iaas/Content/ContEng/Tasks/contengdownloadkubeconfigfile.htm#notes
 provider "helm" {
   kubernetes {
-    load_config_file       = "false"
     cluster_ca_certificate = base64decode(yamldecode(data.oci_containerengine_cluster_kube_config.oke_cluster_kube_config.content)["clusters"][0]["cluster"]["certificate-authority-data"])
     host                   = yamldecode(data.oci_containerengine_cluster_kube_config.oke_cluster_kube_config.content)["clusters"][0]["cluster"]["server"]
     exec {
