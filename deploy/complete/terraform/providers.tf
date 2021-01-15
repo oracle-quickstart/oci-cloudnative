a31ef26aea9175b48b379db58b246250217d0656
Adao Junior <junior@users.noreply.github.com>
2021-01-15 16:50:16 -0600

    providers consolidation

--- deploy/complete/terraform/providers.tf
+++ deploy/complete/terraform/providers.tf
@@ -43,3 +43,27 @@ provider "helm" {
     }
   }
 }
+
+terraform {
+  required_providers {
+    helm = {
+      source = "hashicorp/helm"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+    local = {
+      source = "hashicorp/local"
+    }
+    oci = {
+      source = "hashicorp/oci"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+    tls = {
+      source = "hashicorp/tls"
+    }
+  }
+  required_version = ">= 0.13"
+}
