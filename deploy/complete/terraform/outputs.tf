9dc2462c2de767233976ae4bdd7f03a25dc610b8
Adao Junior <junior@users.noreply.github.com>
2021-03-08 19:34:16 -0600

    new output for ingress

--- deploy/complete/terraform/outputs.tf
+++ deploy/complete/terraform/outputs.tf
@@ -3,19 +3,33 @@
 # 
 
 output "mushop_url_button" {
-  value       = format("http://%s", data.kubernetes_service.mushop_ingress.load_balancer_ingress[0].ip)
+  value       = format("http://%s", local.mushop_ingress_ip)
   description = "MuShop Storefront URL for ORM button"
+
+  depends_on = [helm_release.ingress_nginx]
 }
 output "mushop_url" {
-  value       = format("http://%s", data.kubernetes_service.mushop_ingress.load_balancer_ingress[0].ip)
+  value       = format("http://%s", local.mushop_ingress_ip)
   description = "MuShop Storefront URL"
+
+  depends_on = [helm_release.ingress_nginx]
+}
+output "mushop_url_https" {
+  value       = format("https://%s", local.mushop_ingress_hostname)
+  description = "MuShop Storefront Hostname"
+
+  depends_on = [helm_release.ingress_nginx]
 }
 output "grafana_url" {
-  value       = format("http://%s/grafana", data.kubernetes_service.mushop_ingress.load_balancer_ingress[0].ip)
+  value       = format("http://%s/grafana", local.mushop_ingress_ip)
   description = "Grafana Dashboards URL"
+
+  depends_on = [helm_release.ingress_nginx]
 }
 output "external_ip" {
-  value = data.kubernetes_service.mushop_ingress.load_balancer_ingress[0].ip
+  value = local.mushop_ingress_ip
+
+  depends_on = [helm_release.ingress_nginx]
 }
 output "autonomous_database_password" {
   value     = random_string.autonomous_database_wallet_password.result
@@ -48,4 +62,7 @@ output "kubeconfig_for_kubectl" {
 output "mushop_source_code" {
   value = "https://github.com/oracle-quickstart/oci-cloudnative/tree/master/deploy/complete"
 }
-
+locals {
+    mushop_ingress_ip = data.kubernetes_service.mushop_ingress.status.0.load_balancer.0.ingress.0.ip
+    mushop_ingress_hostname = data.kubernetes_service.mushop_ingress.status.0.load_balancer.0.ingress.0.hostname == "" ? data.kubernetes_service.mushop_ingress.status.0.load_balancer.0.ingress.0.ip : data.kubernetes_service.mushop_ingress.status.0.load_balancer.0.ingress.0.hostname
+}
