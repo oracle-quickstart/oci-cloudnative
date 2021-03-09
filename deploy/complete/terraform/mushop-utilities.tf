3c31ca54c47c0e6e20f233962e3098051c549a52
Adao Junior <junior@users.noreply.github.com>
2021-03-08 19:34:40 -0600

    utilitites updates
    version bumps

--- deploy/complete/terraform/mushop-utilities.tf
+++ deploy/complete/terraform/mushop-utilities.tf
@@ -18,7 +18,7 @@ resource "helm_release" "prometheus" {
   name       = "prometheus"
   repository = local.helm_repository.prometheus
   chart      = "prometheus"
-  version    = "13.2.1"
+  version    = "13.5.0"
   namespace  = kubernetes_namespace.mushop_utilities_namespace.id
   wait       = false
 
@@ -37,7 +37,7 @@ resource "helm_release" "grafana" {
   name       = "mushop-utils-grafana" # mushop-utils included to be backwards compatible to the docs and setup chart install
   repository = local.helm_repository.grafana
   chart      = "grafana"
-  version    = "6.1.17"
+  version    = "6.4.8"
   namespace  = kubernetes_namespace.mushop_utilities_namespace.id
   wait       = false
 
@@ -74,7 +74,7 @@ resource "helm_release" "ingress_nginx" {
   name       = "mushop-utils-ingress-nginx" # mushop-utils included to be backwards compatible to the docs and setup chart install
   repository = local.helm_repository.ingress_nginx
   chart      = "ingress-nginx"
-  version    = "3.20.1"
+  version    = "3.23.0"
   namespace  = kubernetes_namespace.mushop_utilities_namespace.id
   wait       = true
 
@@ -113,7 +113,7 @@ resource "helm_release" "cert_manager" {
   name       = "cert-manager"
   repository = local.helm_repository.jetstack
   chart      = "cert-manager"
-  version    = "1.1.0"
+  version    = "1.2.0"
   namespace  = kubernetes_namespace.mushop_utilities_namespace.id
   wait       = false
 
