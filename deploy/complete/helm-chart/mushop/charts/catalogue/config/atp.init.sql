fd0a66afcb846f7f03acff3ad9261a074932298e
Adao Junior <junior@users.noreply.github.com>
2021-03-11 22:55:55 -0600

    custom db profile for password

--- deploy/complete/helm-chart/mushop/charts/catalogue/config/atp.init.sql
+++ deploy/complete/helm-chart/mushop/charts/catalogue/config/atp.init.sql
@@ -1,9 +1,35 @@
--- Copyright (c) 2019 Oracle and/or its affiliates. All rights reserved.
+-- Copyright (c) 2019, 2021 Oracle and/or its affiliates. All rights reserved.
 -- Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
 
 SET SERVEROUTPUT ON;
 SET VERIFY OFF;
 BEGIN
+	-- Profile Creation
+	DECLARE
+		profileExists INTEGER;
+	BEGIN
+		SELECT COUNT(*) 
+		INTO profileExists 
+		FROM DBA_PROFILES 
+		WHERE PROFILE = 'mushop_services';
+		DBMS_OUTPUT.PUT_LINE ('** Profile creationg steps - &_DATE');
+		IF profileExists = 0 THEN
+			DBMS_OUTPUT.PUT_LINE ('Creating Profile = mushop_services ...');
+			EXECUTE IMMEDIATE
+				'CREATE PROFILE mushop_services
+					LIMIT	PASSWORD_LIFE_TIME UNLIMITED
+							PASSWORD_GRACE_TIME 0
+							PASSWORD_REUSE_TIME UNLIMITED
+							PASSWORD_REUSE_MAX UNLIMITED
+							PASSWORD_LOCK_TIME 1
+							FAILED_LOGIN_ATTEMPTS UNLIMITED
+							PASSWORD_LOCK_TIME UNLIMITED
+							INACTIVE_ACCOUNT_TIME UNLIMITED';
+		ELSE
+			DBMS_OUTPUT.PUT_LINE ('Profile = mushop_services exists, steps ignored');
+		END IF;
+	END;
+
 	-- Schema User Creation
 	DECLARE
 		schemaUserExists INTEGER;
@@ -15,7 +41,7 @@ BEGIN
 		DBMS_OUTPUT.PUT_LINE ('** Schema creationg steps - &_DATE');
 		IF schemaUserExists = 0 THEN
 			DBMS_OUTPUT.PUT_LINE ('Creating schema = &1 ...');
-			EXECUTE IMMEDIATE 'CREATE USER &1 IDENTIFIED BY &2';
+			EXECUTE IMMEDIATE 'CREATE USER &1 IDENTIFIED BY &2 PROFILE mushop_services';
 			EXECUTE IMMEDIATE 'GRANT CREATE SESSION TO &1';
 			EXECUTE IMMEDIATE 'GRANT UNLIMITED TABLESPACE TO &1';
 		ELSE
