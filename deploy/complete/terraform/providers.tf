12e517c83d76ff43e70376c56378cc9c0d358236
Adao Junior <junior@users.noreply.github.com>
2021-03-14 22:26:33 -0500

    allow get home provider for identity resources

--- deploy/complete/terraform/providers.tf
+++ deploy/complete/terraform/providers.tf
@@ -37,6 +37,26 @@ provider "oci" {
   region       = var.region
 }
 
+provider "oci" {
+  alias        = "home_region"
+  tenancy_ocid = var.tenancy_ocid
+  region       = lookup(data.oci_identity_regions.home_region.regions[0], "name")
+
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
+}
+
+provider "oci" {
+  alias        = "current_region"
+  tenancy_ocid = var.tenancy_ocid
+  region       = var.region
+
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
+}
+
 # https://docs.cloud.oracle.com/en-us/iaas/Content/ContEng/Tasks/contengdownloadkubeconfigfile.htm#notes
 provider "kubernetes" {
   load_config_file       = "false" # Workaround for tf k8s provider < 1.11.1 to work with ORM
