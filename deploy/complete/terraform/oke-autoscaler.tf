2885a5d7d9cac02bd2e0ec77e403b4ec88896f78
Adao Junior <junior@users.noreply.github.com>
2021-03-14 22:27:08 -0500

    introduction to the clsuter autoscaler tf script

--- deploy/complete/terraform/oke-autoscaler.tf
+++ deploy/complete/terraform/oke-autoscaler.tf
@@ -255,34 +255,3 @@ resource "kubernetes_deployment" "cluster_autoscaler_deployment" {
 
   depends_on = [oci_containerengine_node_pool.oke_mushop_node_pool]
 }
-
-resource "oci_identity_dynamic_group" "cluster_autoscaler_dg" {
-  name           = "cluster-autoscaler-dg-${random_string.deploy_id.result}"
-  description    = "Cluster Autoscaler Dynamic Group"
-  compartment_id = var.tenancy_ocid
-  matching_rule  = "ALL {instance.compartment.id = '${var.compartment_ocid}'}"
-
-  #   provider = oci.home_region
-}
-resource "oci_identity_policy" "cluster_autoscaler_policies" {
-  name           = "cluster-autoscaler-policies-${random_string.deploy_id.result}"
-  description    = "Cluster Autoscaler Policies"
-  compartment_id = var.compartment_ocid
-  statements     = local.cluster_autoscaler_policies_statement
-
-  depends_on = [oci_identity_dynamic_group.cluster_autoscaler_dg]
-
-  #   provider = oci.home_region
-}
-
-locals {
-
-  cluster_autoscaler_policies_statement = [
-    "Allow dynamic-group ${oci_identity_dynamic_group.cluster_autoscaler_dg.name} to manage cluster-node-pools in compartment id ${var.compartment_ocid}",
-    "Allow dynamic-group ${oci_identity_dynamic_group.cluster_autoscaler_dg.name} to manage instance-family in compartment id ${var.compartment_ocid}",
-    "Allow dynamic-group ${oci_identity_dynamic_group.cluster_autoscaler_dg.name} to use subnets in compartment id ${var.compartment_ocid}",
-    "Allow dynamic-group ${oci_identity_dynamic_group.cluster_autoscaler_dg.name} to read virtual-network-family in compartment id ${var.compartment_ocid}",
-    "Allow dynamic-group ${oci_identity_dynamic_group.cluster_autoscaler_dg.name} to use vnics in compartment id ${var.compartment_ocid}",
-    "Allow dynamic-group ${oci_identity_dynamic_group.cluster_autoscaler_dg.name} to inspect compartments in compartment id ${var.compartment_ocid}"
-  ]
-}
