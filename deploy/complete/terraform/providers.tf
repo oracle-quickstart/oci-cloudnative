d2c532315c58484cca8212caeccd6a4a02d9ffe0
Adao Junior <junior@users.noreply.github.com>
2021-03-14 20:56:14 -0500

    retro support of provider for ORM

--- deploy/complete/terraform/providers.tf
+++ deploy/complete/terraform/providers.tf
@@ -2,6 +2,36 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
 # 
 
+terraform {
+  required_version = ">= 0.14"
+  required_providers {
+    oci = {
+      source = "hashicorp/oci"
+      version = ">= 4.17.0"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "1.11.2" # Latest version as March 2021 = 2.0.2. Using 1.11.2 (March, 2020) for ORM compatibility
+    }
+    helm = {
+      source = "hashicorp/helm"
+      version = "1.1.1" # Latest version as March 2021 = 2.0.3. Using 1.1.1 (March, 2020) for ORM compatibility
+    }
+    tls = {
+      source = "hashicorp/tls"
+      version = "2.0.1" # Latest version as March 2021 = 3.1.0. Using 2.0.1 (April, 2020) for ORM compatibility
+    }
+    local = {
+      source = "hashicorp/local"
+      version = "1.4.0" # Latest version as March 2021 = 2.1.0. Using 1.4.0 (September, 2019) for ORM compatibility
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "2.3.0" # Latest version as March 2021 = 3.1.0. Using 2.3.0 (July, 2020) for ORM compatibility
+    }
+  }
+}
+
 provider "oci" {
   tenancy_ocid = var.tenancy_ocid
   region       = var.region
@@ -9,7 +39,7 @@ provider "oci" {
 
 # https://docs.cloud.oracle.com/en-us/iaas/Content/ContEng/Tasks/contengdownloadkubeconfigfile.htm#notes
 provider "kubernetes" {
-  # load_config_file       = "false"
+  load_config_file       = "false" # Workaround for tf k8s provider < 1.11.1 to work with ORM
   cluster_ca_certificate = base64decode(yamldecode(data.oci_containerengine_cluster_kube_config.oke_cluster_kube_config.content)["clusters"][0]["cluster"]["certificate-authority-data"])
   host                   = yamldecode(data.oci_containerengine_cluster_kube_config.oke_cluster_kube_config.content)["clusters"][0]["cluster"]["server"]
   exec {
@@ -28,6 +58,7 @@ provider "kubernetes" {
 # https://docs.cloud.oracle.com/en-us/iaas/Content/ContEng/Tasks/contengdownloadkubeconfigfile.htm#notes
 provider "helm" {
   kubernetes {
+    load_config_file       = "false" # Workaround for tf helm provider < 1.1.1 to work with ORM
     cluster_ca_certificate = base64decode(yamldecode(data.oci_containerengine_cluster_kube_config.oke_cluster_kube_config.content)["clusters"][0]["cluster"]["certificate-authority-data"])
     host                   = yamldecode(data.oci_containerengine_cluster_kube_config.oke_cluster_kube_config.content)["clusters"][0]["cluster"]["server"]
     exec {
@@ -42,28 +73,4 @@ provider "helm" {
       command = yamldecode(data.oci_containerengine_cluster_kube_config.oke_cluster_kube_config.content)["users"][0]["user"]["exec"]["command"]
     }
   }
-}
-
-terraform {
-  required_providers {
-    helm = {
-      source = "hashicorp/helm"
-    }
-    kubernetes = {
-      source = "hashicorp/kubernetes"
-    }
-    local = {
-      source = "hashicorp/local"
-    }
-    oci = {
-      source = "hashicorp/oci"
-    }
-    random = {
-      source = "hashicorp/random"
-    }
-    tls = {
-      source = "hashicorp/tls"
-    }
-  }
-  required_version = ">= 0.14"
 }
