287008d728f39e95d5c6d680767118070d851846
Adao Junior <junior@users.noreply.github.com>
2021-03-30 01:52:52 -0500

    resource names simplification 1

--- deploy/complete/terraform/policies.tf
+++ deploy/complete/terraform/policies.tf
@@ -2,7 +2,7 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
 # 
 
-resource "oci_identity_dynamic_group" "oke_mushop_dg" {
+resource "oci_identity_dynamic_group" "oke_nodes_dg" {
   count          = var.create_dynamic_group_for_nodes_in_compartment ? 1 : 0
   name           = "mushop-oke-cluster-dg-${random_string.deploy_id.result}"
   description    = "MuShop Cluster Dynamic Group"
@@ -18,7 +18,7 @@ resource "oci_identity_policy" "mushop_oke_compartment_policies" {
   compartment_id = local.oke_compartment_ocid
   statements     = local.mushop_policies_statement
 
-  depends_on = [oci_identity_dynamic_group.oke_mushop_dg]
+  depends_on = [oci_identity_dynamic_group.oke_nodes_dg]
 
   provider = oci.home_region
 }
@@ -32,19 +32,19 @@ locals {
 
 locals {
   oci_grafana_metrics_policies_statement = [
-    "Allow dynamic-group ${oci_identity_dynamic_group.oke_mushop_dg.0.name} to read metrics in compartment id ${local.oke_compartment_ocid}",
-    "Allow dynamic-group ${oci_identity_dynamic_group.oke_mushop_dg.0.name} to read compartments in compartment id ${local.oke_compartment_ocid}"
+    "Allow dynamic-group ${oci_identity_dynamic_group.oke_nodes_dg.0.name} to read metrics in compartment id ${local.oke_compartment_ocid}",
+    "Allow dynamic-group ${oci_identity_dynamic_group.oke_nodes_dg.0.name} to read compartments in compartment id ${local.oke_compartment_ocid}"
   ]
   oci_grafana_logs_policies_statement = [
-    "Allow dynamic-group ${oci_identity_dynamic_group.oke_mushop_dg.0.name} to read log-groups in compartment id ${local.oke_compartment_ocid}",
-    "Allow dynamic-group ${oci_identity_dynamic_group.oke_mushop_dg.0.name} to read log-content in compartment id ${local.oke_compartment_ocid}"
+    "Allow dynamic-group ${oci_identity_dynamic_group.oke_nodes_dg.0.name} to read log-groups in compartment id ${local.oke_compartment_ocid}",
+    "Allow dynamic-group ${oci_identity_dynamic_group.oke_nodes_dg.0.name} to read log-content in compartment id ${local.oke_compartment_ocid}"
   ]
   cluster_autoscaler_policies_statement = [
-    "Allow dynamic-group ${oci_identity_dynamic_group.oke_mushop_dg.0.name} to manage cluster-node-pools in compartment id ${local.oke_compartment_ocid}",
-    "Allow dynamic-group ${oci_identity_dynamic_group.oke_mushop_dg.0.name} to manage instance-family in compartment id ${local.oke_compartment_ocid}",
-    "Allow dynamic-group ${oci_identity_dynamic_group.oke_mushop_dg.0.name} to use subnets in compartment id ${local.oke_compartment_ocid}",
-    "Allow dynamic-group ${oci_identity_dynamic_group.oke_mushop_dg.0.name} to read virtual-network-family in compartment id ${local.oke_compartment_ocid}",
-    "Allow dynamic-group ${oci_identity_dynamic_group.oke_mushop_dg.0.name} to use vnics in compartment id ${local.oke_compartment_ocid}",
-    "Allow dynamic-group ${oci_identity_dynamic_group.oke_mushop_dg.0.name} to inspect compartments in compartment id ${local.oke_compartment_ocid}"
+    "Allow dynamic-group ${oci_identity_dynamic_group.oke_nodes_dg.0.name} to manage cluster-node-pools in compartment id ${local.oke_compartment_ocid}",
+    "Allow dynamic-group ${oci_identity_dynamic_group.oke_nodes_dg.0.name} to manage instance-family in compartment id ${local.oke_compartment_ocid}",
+    "Allow dynamic-group ${oci_identity_dynamic_group.oke_nodes_dg.0.name} to use subnets in compartment id ${local.oke_compartment_ocid}",
+    "Allow dynamic-group ${oci_identity_dynamic_group.oke_nodes_dg.0.name} to read virtual-network-family in compartment id ${local.oke_compartment_ocid}",
+    "Allow dynamic-group ${oci_identity_dynamic_group.oke_nodes_dg.0.name} to use vnics in compartment id ${local.oke_compartment_ocid}",
+    "Allow dynamic-group ${oci_identity_dynamic_group.oke_nodes_dg.0.name} to inspect compartments in compartment id ${local.oke_compartment_ocid}"
   ]
 }
