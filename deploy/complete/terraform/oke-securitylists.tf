6af91df0a9e59dc74ac497b4020adf607a89c5cb
Adao Junior <junior@users.noreply.github.com>
2021-03-30 01:31:04 -0500

    oke security lists initial changes

--- deploy/complete/terraform/oke-securitylists.tf
+++ deploy/complete/terraform/oke-securitylists.tf
@@ -1,16 +1,46 @@
-# Copyright (c) 2020 Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2020, 2021 Oracle and/or its affiliates. All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
 # 
 
 resource "oci_core_security_list" "oke_mushop_security_list" {
-  compartment_id = var.compartment_ocid
+  compartment_id = local.oke_compartment_ocid
   display_name   = "oke-mushop-wkr-seclist-${random_string.deploy_id.result}"
   vcn_id         = oci_core_virtual_network.oke_mushop_vcn[0].id
 
+  ingress_security_rules {
+    description = "Allow pods on one worker node to communicate with pods on other worker nodes"
+    source      = lookup(var.network_cidrs, "SUBNET-REGIONAL-CIDR")
+    source_type = "CIDR_BLOCK"
+    protocol    = local.all_protocols
+    stateless   = true
+  }
+
+  ingress_security_rules {
+    description = "Inbound SSH traffic to worker nodes"
+    source      = lookup(var.network_cidrs, (var.cluster_workers_visibility == "Private") ? "VCN-CIDR" : "ALL-CIDR")
+    source_type = "CIDR_BLOCK"
+    protocol    = local.tcp_protocol_number
+    stateless   = false
+
+    tcp_options {
+      max = local.ssh_port_number
+      min = local.ssh_port_number
+    }
+  }
+
+  ingress_security_rules {
+    description = "TCP access from Kubernetes Control Plane"
+    source      = lookup(var.network_cidrs, "ENDPOINT-SUBNET-REGIONAL-CIDR")
+    source_type = "CIDR_BLOCK"
+    protocol    = local.tcp_protocol_number
+    stateless   = false
+  }
+
   egress_security_rules {
+    description = "Allow pods on one worker node to communicate with pods on other worker nodes"
     destination      = lookup(var.network_cidrs, "SUBNET-REGIONAL-CIDR")
     destination_type = "CIDR_BLOCK"
-    protocol         = "all"
+    protocol         = local.all_protocols
     stateless        = true
   }
 
@@ -27,30 +57,11 @@ resource "oci_core_security_list" "oke_mushop_security_list" {
     protocol         = "all"
   }
 
-  ingress_security_rules {
-    source      = lookup(var.network_cidrs, "SUBNET-REGIONAL-CIDR")
-    source_type = "CIDR_BLOCK"
-    protocol    = "all"
-    stateless   = true
-  }
-
-  ingress_security_rules {
-    source      = lookup(var.network_cidrs, "VCN-CIDR") # "10.0.0.0/16"
-    source_type = "CIDR_BLOCK"
-    protocol    = "6"
-    stateless   = false
-
-    tcp_options {
-      max = "22"
-      min = "22"
-    }
-  }
-
   count = var.create_new_oke_cluster ? 1 : 0
 }
 
 resource "oci_core_security_list" "oke_mushop_lb_security_list" {
-  compartment_id = var.compartment_ocid
+  compartment_id = local.oke_compartment_ocid
   display_name   = "oke-mushop-wkr-lb-seclist-${random_string.deploy_id.result}"
   vcn_id         = oci_core_virtual_network.oke_mushop_vcn[0].id
 
@@ -69,4 +80,14 @@ resource "oci_core_security_list" "oke_mushop_lb_security_list" {
   }
 
   count = var.create_new_oke_cluster ? 1 : 0
+}
+
+locals {
+  http_port_number          = "80"
+  https_port_number         = "443"
+  k8s_api_endpoint_port_number = "6443"
+  k8s_worker_to_control_plane_port_number = "12250"
+  ssh_port_number           = "22"
+  tcp_protocol_number       = "6"
+  all_protocols             = "all"
 }
