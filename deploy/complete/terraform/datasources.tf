c344bfc253df549889e57a1e9e1d4d95a28eb2fd
Adao Junior <junior@users.noreply.github.com>
2021-03-30 01:30:26 -0500

    ds new compartment

--- deploy/complete/terraform/datasources.tf
+++ deploy/complete/terraform/datasources.tf
@@ -4,7 +4,7 @@
 
 # Gets a list of supported images based on the shape, operating_system and operating_system_version provided
 data "oci_core_images" "node_pool_images" {
-  compartment_id           = var.compartment_ocid
+  compartment_id           = local.oke_compartment_ocid
   operating_system         = var.image_operating_system
   operating_system_version = var.image_operating_system_version
   shape                    = var.node_pool_shape
@@ -82,7 +82,7 @@ data "oci_core_services" "all_services" {
 
 ## Object Storage
 data "oci_objectstorage_namespace" "ns" {
-  compartment_id = var.compartment_ocid
+  compartment_id = local.oke_compartment_ocid
 }
 
 # Randoms
