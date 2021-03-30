e4b74fe0fb679b0bf92c34f8d351fa91da304315
Adao Junior <junior@users.noreply.github.com>
2021-03-30 01:53:18 -0500

    resource names simplification 4

--- deploy/complete/terraform/oke-network.tf
+++ deploy/complete/terraform/oke-network.tf
@@ -18,41 +18,41 @@ resource "oci_core_subnet" "oke_k8s_endpoint_subnet" {
   dns_label                  = "okek8ssubnet${random_string.deploy_id.result}"
   vcn_id                     = oci_core_virtual_network.oke_vcn[0].id
   prohibit_public_ip_on_vnic = (var.cluster_endpoint_visibility == "Private") ? true : false
-  route_table_id             = oci_core_route_table.oke_mushop_route_table[0].id
+  route_table_id             = oci_core_route_table.oke_route_table[0].id
   dhcp_options_id            = oci_core_virtual_network.oke_vcn[0].default_dhcp_options_id
-  security_list_ids          = [oci_core_security_list.oke_mushop_security_list[0].id]
+  security_list_ids          = [oci_core_security_list.oke_nodes_security_list[0].id]
 
   count = var.create_new_oke_cluster ? 1 : 0
 }
-resource "oci_core_subnet" "oke_mushop_subnet" {
+resource "oci_core_subnet" "oke_nodes_subnet" {
   cidr_block                 = lookup(var.network_cidrs, "SUBNET-REGIONAL-CIDR")
   compartment_id             = local.oke_compartment_ocid
   display_name               = "oke-mushop-subnet-${random_string.deploy_id.result}"
   dns_label                  = "okesubnet${random_string.deploy_id.result}"
   vcn_id                     = oci_core_virtual_network.oke_vcn[0].id
   prohibit_public_ip_on_vnic = (var.cluster_workers_visibility == "Private") ? true : false
-  route_table_id             = oci_core_route_table.oke_mushop_route_table[0].id
+  route_table_id             = oci_core_route_table.oke_route_table[0].id
   dhcp_options_id            = oci_core_virtual_network.oke_vcn[0].default_dhcp_options_id
-  security_list_ids          = [oci_core_security_list.oke_mushop_security_list[0].id]
+  security_list_ids          = [oci_core_security_list.oke_nodes_security_list[0].id]
 
   count = var.create_new_oke_cluster ? 1 : 0
 }
 
-resource "oci_core_subnet" "oke_mushop_lb_subnet" {
+resource "oci_core_subnet" "oke_lb_subnet" {
   cidr_block                 = lookup(var.network_cidrs, "LB-SUBNET-REGIONAL-CIDR")
   compartment_id             = local.oke_compartment_ocid
   display_name               = "oke-mushop-lb-subnet-${random_string.deploy_id.result}"
   dns_label                  = "okelbsubnet${random_string.deploy_id.result}"
   vcn_id                     = oci_core_virtual_network.oke_vcn[0].id
   prohibit_public_ip_on_vnic = false
-  route_table_id             = oci_core_route_table.oke_mushop_lb_route_table[0].id
+  route_table_id             = oci_core_route_table.oke_lb_route_table[0].id
   dhcp_options_id            = oci_core_virtual_network.oke_vcn[0].default_dhcp_options_id
-  security_list_ids          = [oci_core_security_list.oke_mushop_lb_security_list[0].id]
+  security_list_ids          = [oci_core_security_list.oke_lb_security_list[0].id]
 
   count = var.create_new_oke_cluster ? 1 : 0
 }
 
-resource "oci_core_route_table" "oke_mushop_route_table" {
+resource "oci_core_route_table" "oke_route_table" {
   compartment_id = local.oke_compartment_ocid
   vcn_id         = oci_core_virtual_network.oke_vcn[0].id
   display_name   = "oke-mushop-route-table-${random_string.deploy_id.result}"
@@ -60,13 +60,13 @@ resource "oci_core_route_table" "oke_mushop_route_table" {
   route_rules {
     destination       = lookup(var.network_cidrs, "ALL-CIDR")
     destination_type  = "CIDR_BLOCK"
-    network_entity_id = (var.cluster_workers_visibility == "Private") ? oci_core_nat_gateway.oke_mushop_nat_gateway[0].id : oci_core_internet_gateway.oke_mushop_internet_gateway[0].id
+    network_entity_id = (var.cluster_workers_visibility == "Private") ? oci_core_nat_gateway.oke_nat_gateway[0].id : oci_core_internet_gateway.oke_internet_gateway[0].id
   }
 
   count = var.create_new_oke_cluster ? 1 : 0
 }
 
-resource "oci_core_route_table" "oke_mushop_lb_route_table" {
+resource "oci_core_route_table" "oke_lb_route_table" {
   compartment_id = local.oke_compartment_ocid
   vcn_id         = oci_core_virtual_network.oke_vcn[0].id
   display_name   = "oke-mushop-lb-route-table-${random_string.deploy_id.result}"
@@ -74,13 +74,13 @@ resource "oci_core_route_table" "oke_mushop_lb_route_table" {
   route_rules {
     destination       = lookup(var.network_cidrs, "ALL-CIDR")
     destination_type  = "CIDR_BLOCK"
-    network_entity_id = oci_core_internet_gateway.oke_mushop_internet_gateway[0].id
+    network_entity_id = oci_core_internet_gateway.oke_internet_gateway[0].id
   }
 
   count = var.create_new_oke_cluster ? 1 : 0
 }
 
-resource "oci_core_nat_gateway" "oke_mushop_nat_gateway" {
+resource "oci_core_nat_gateway" "oke_nat_gateway" {
   block_traffic  = "false"
   compartment_id = local.oke_compartment_ocid
   display_name   = "oke-mushop-nat-gateway-${random_string.deploy_id.result}"
@@ -89,7 +89,7 @@ resource "oci_core_nat_gateway" "oke_mushop_nat_gateway" {
   count = var.create_new_oke_cluster ? ((var.cluster_workers_visibility == "Private") ? 1 : 0) : 0
 }
 
-resource "oci_core_internet_gateway" "oke_mushop_internet_gateway" {
+resource "oci_core_internet_gateway" "oke_internet_gateway" {
   compartment_id = local.oke_compartment_ocid
   display_name   = "oke-mushop-internet-gateway-${random_string.deploy_id.result}"
   enabled        = true
@@ -98,7 +98,7 @@ resource "oci_core_internet_gateway" "oke_mushop_internet_gateway" {
   count = var.create_new_oke_cluster ? 1 : 0
 }
 
-resource "oci_core_service_gateway" "oke_mushop_service_gateway" {
+resource "oci_core_service_gateway" "oke_service_gateway" {
   compartment_id = local.oke_compartment_ocid
   display_name   = "oke-mushop-service-gateway-${random_string.deploy_id.result}"
   vcn_id         = oci_core_virtual_network.oke_vcn[0].id
