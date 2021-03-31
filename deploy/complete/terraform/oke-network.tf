890156775f08c504b85e8003aac7faa0d2442c40
Adao Junior <junior@users.noreply.github.com>
2021-03-30 21:09:07 -0500

    deprecating single route_table

--- deploy/complete/terraform/oke-network.tf
+++ deploy/complete/terraform/oke-network.tf
@@ -18,7 +18,7 @@ resource "oci_core_subnet" "oke_k8s_endpoint_subnet" {
   dns_label                  = "okek8ssubnet${random_string.deploy_id.result}"
   vcn_id                     = oci_core_virtual_network.oke_vcn[0].id
   prohibit_public_ip_on_vnic = (var.cluster_endpoint_visibility == "Private") ? true : false
-  route_table_id             = oci_core_route_table.oke_route_table[0].id
+  route_table_id             = (var.cluster_endpoint_visibility == "Private") ? oci_core_route_table.oke_private_route_table[0].id : oci_core_route_table.oke_public_route_table[0].id
   dhcp_options_id            = oci_core_virtual_network.oke_vcn[0].default_dhcp_options_id
   security_list_ids          = [oci_core_security_list.oke_endpoint_security_list[0].id]
 
@@ -31,7 +31,7 @@ resource "oci_core_subnet" "oke_nodes_subnet" {
   dns_label                  = "okesubnet${random_string.deploy_id.result}"
   vcn_id                     = oci_core_virtual_network.oke_vcn[0].id
   prohibit_public_ip_on_vnic = (var.cluster_workers_visibility == "Private") ? true : false
-  route_table_id             = oci_core_route_table.oke_route_table[0].id
+  route_table_id             = (var.cluster_workers_visibility == "Private") ? oci_core_route_table.oke_private_route_table[0].id : oci_core_route_table.oke_public_route_table[0].id
   dhcp_options_id            = oci_core_virtual_network.oke_vcn[0].default_dhcp_options_id
   security_list_ids          = [oci_core_security_list.oke_nodes_security_list[0].id]
 
@@ -48,20 +48,6 @@ resource "oci_core_subnet" "oke_lb_subnet" {
   route_table_id             = oci_core_route_table.oke_public_route_table[0].id
   dhcp_options_id            = oci_core_virtual_network.oke_vcn[0].default_dhcp_options_id
   security_list_ids          = [oci_core_security_list.oke_lb_security_list[0].id]
-
-  count = var.create_new_oke_cluster ? 1 : 0
-}
-
-resource "oci_core_route_table" "oke_route_table" {
-  compartment_id = local.oke_compartment_ocid
-  vcn_id         = oci_core_virtual_network.oke_vcn[0].id
-  display_name   = "oke-route-table-${lower(var.app_name)}-${random_string.deploy_id.result}"
-
-  route_rules {
-    destination       = lookup(var.network_cidrs, "ALL-CIDR")
-    destination_type  = "CIDR_BLOCK"
-    network_entity_id = (var.cluster_workers_visibility == "Private") ? oci_core_nat_gateway.oke_nat_gateway[0].id : oci_core_internet_gateway.oke_internet_gateway[0].id
-  }
 
   count = var.create_new_oke_cluster ? 1 : 0
 }
