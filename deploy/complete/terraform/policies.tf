cfa1c41631fb2f3942dfe48cb782e98624631f6a
Adao Junior <junior@users.noreply.github.com>
2021-04-01 16:41:18 -0500

    separation of policies (compartment vs tenancy)

--- deploy/complete/terraform/policies.tf
+++ deploy/complete/terraform/policies.tf
@@ -13,8 +13,8 @@ resource "oci_identity_dynamic_group" "oke_nodes_dg" {
   count = var.create_dynamic_group_for_nodes_in_compartment ? 1 : 0
 }
 resource "oci_identity_policy" "oke_compartment_policies" {
-  name           = "${lower(trimspace(var.app_name))}-oke-cluster-policies-${random_string.deploy_id.result}"
-  description    = "${var.app_name} OKE Cluster Policies"
+  name           = "${lower(trimspace(var.app_name))}-oke-cluster-compartment-policies-${random_string.deploy_id.result}"
+  description    = "${var.app_name} OKE Cluster Compartment Policies"
   compartment_id = local.oke_compartment_ocid
   statements     = local.oke_compartment_policies_statement
 
@@ -25,18 +25,33 @@ resource "oci_identity_policy" "oke_compartment_policies" {
   count = var.create_compartment_policies ? 1 : 0
 }
 
+resource "oci_identity_policy" "oke_tenancy_policies" {
+  name           = "${lower(trimspace(var.app_name))}-oke-cluster-tenancy-policies-${random_string.deploy_id.result}"
+  description    = "${var.app_name} OKE Cluster Tenancy Policies"
+  compartment_id = var.tenancy_ocid
+  statements     = local.oke_tenancy_policies_statement
+
+  depends_on = [oci_identity_dynamic_group.oke_nodes_dg]
+
+  provider = oci.home_region
+
+  count = var.create_tenancy_policies ? 1 : 0
+}
+
 locals {
   oke_compartment_policies_statement = concat(
-    local.oci_grafana_metrics_policies_statement,
     local.oci_grafana_logs_policies_statement
+  )
+  oke_tenancy_policies_statement = concat(
+    local.oci_grafana_metrics_policies_statement
   )
 }
 
 locals {
   oke_nodes_dg = var.create_dynamic_group_for_nodes_in_compartment ? oci_identity_dynamic_group.oke_nodes_dg.0.name : "void"
   oci_grafana_metrics_policies_statement = [
-    "Allow dynamic-group ${local.oke_nodes_dg} to read metrics in compartment id ${local.oke_compartment_ocid}",
-    "Allow dynamic-group ${local.oke_nodes_dg} to read compartments in compartment id ${local.oke_compartment_ocid}"
+    "Allow dynamic-group ${local.oke_nodes_dg} to read metrics in tenancy",
+    "Allow dynamic-group ${local.oke_nodes_dg} to read compartments in tenancy"
   ]
   oci_grafana_logs_policies_statement = [
     "Allow dynamic-group ${local.oke_nodes_dg} to read log-groups in compartment id ${local.oke_compartment_ocid}",
