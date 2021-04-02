9cb33e4267415acc2356625438a49f976cc1122b
Adao Junior <junior@users.noreply.github.com>
2021-04-02 15:51:57 -0500

    KMS resources

--- deploy/complete/terraform/mushop-oci-services.tf
+++ deploy/complete/terraform/mushop-oci-services.tf
@@ -210,3 +210,29 @@ resource "kubernetes_secret" "oos_bucket" {
 
   count = var.mushop_mock_mode_all ? 0 : 1
 }
+
+## OCI KMS Vault
+### OCI Vault vault
+resource "oci_kms_vault" "mushop_vault" {
+  compartment_id = var.compartment_ocid
+  display_name   = "${var.vault_display_name} - ${random_string.deploy_id.result}"
+  vault_type     = var.vault_type[0]
+  freeform_tags  = local.common_tags
+
+  count      = var.use_encryption_from_oci_vault ? (var.create_new_encryption_key ? 1 : 0) : 0
+  depends_on = [oci_identity_policy.mushop_basic_policies]
+}
+### OCI Vault key
+resource "oci_kms_key" "mushop_key" {
+  compartment_id      = var.compartment_ocid
+  display_name        = "${var.vault_key_display_name} - ${random_string.deploy_id.result}"
+  management_endpoint = oci_kms_vault.mushop_vault[0].management_endpoint
+
+  key_shape {
+    algorithm = var.vault_key_key_shape_algorithm
+    length    = var.vault_key_key_shape_length
+  }
+  freeform_tags = local.common_tags
+
+  count = var.use_encryption_from_oci_vault ? (var.create_new_encryption_key ? 1 : 0) : 0
+}
