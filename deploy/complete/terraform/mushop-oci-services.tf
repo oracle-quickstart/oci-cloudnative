5af0c7b1d2af4193fb4aa3ed32f9dc2f45fd2cb3
Adao Junior <junior@users.noreply.github.com>
2021-04-02 16:11:12 -0500

    kms locals

--- deploy/complete/terraform/mushop-oci-services.tf
+++ deploy/complete/terraform/mushop-oci-services.tf
@@ -214,25 +214,36 @@ resource "kubernetes_secret" "oos_bucket" {
 ## OCI KMS Vault
 ### OCI Vault vault
 resource "oci_kms_vault" "mushop_vault" {
-  compartment_id = var.compartment_ocid
-  display_name   = "${var.vault_display_name} - ${random_string.deploy_id.result}"
-  vault_type     = var.vault_type[0]
+  compartment_id = local.oke_compartment_ocid
+  display_name   = "${local.vault_display_name} - ${random_string.deploy_id.result}"
+  vault_type     = local.vault_type[0]
   freeform_tags  = local.common_tags
 
+  depends_on = [oci_identity_policy.oke_compartment_policies]
+
   count      = var.use_encryption_from_oci_vault ? (var.create_new_encryption_key ? 1 : 0) : 0
-  depends_on = [oci_identity_policy.mushop_basic_policies]
 }
 ### OCI Vault key
 resource "oci_kms_key" "mushop_key" {
-  compartment_id      = var.compartment_ocid
-  display_name        = "${var.vault_key_display_name} - ${random_string.deploy_id.result}"
+  compartment_id      = local.oke_compartment_ocid
+  display_name        = "${local.vault_key_display_name} - ${random_string.deploy_id.result}"
   management_endpoint = oci_kms_vault.mushop_vault[0].management_endpoint
 
   key_shape {
-    algorithm = var.vault_key_key_shape_algorithm
-    length    = var.vault_key_key_shape_length
+    algorithm = local.vault_key_key_shape_algorithm
+    length    = local.vault_key_key_shape_length
   }
   freeform_tags = local.common_tags
 
   count = var.use_encryption_from_oci_vault ? (var.create_new_encryption_key ? 1 : 0) : 0
+}
+
+### Vault and Key definitions
+
+locals {
+  vault_display_name = "MuShop Vault"
+  vault_key_display_name = "MuShop Key"
+  vault_key_key_shape_algorithm = "AES"
+  vault_key_key_shape_length = 32
+  vault_type = ["DEFAULT", "VIRTUAL_PRIVATE"]
 }
