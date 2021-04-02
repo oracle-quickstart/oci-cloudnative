2052948b8f3cbbae82d04481f77ae9897cf223cb
Adao Junior <junior@users.noreply.github.com>
2021-04-02 16:40:40 -0500

    new policy for KMS

--- deploy/complete/terraform/policies.tf
+++ deploy/complete/terraform/policies.tf
@@ -24,6 +24,18 @@ resource "oci_identity_policy" "oke_compartment_policies" {
 
   count = var.create_compartment_policies ? 1 : 0
 }
+resource "oci_identity_policy" "kms_compartment_policies" {
+  name           = "${lower(trimspace(var.app_name))}-kms-compartment-policies-${random_string.deploy_id.result}"
+  description    = "${var.app_name} KMS Compartment Policies"
+  compartment_id = local.oke_compartment_ocid
+  statements     = local.kms_compartment_statements
+
+  depends_on = [oci_identity_dynamic_group.oke_nodes_dg]
+
+  provider = oci.home_region
+
+  count = (var.create_compartment_policies && var.create_vault_policies_for_group) ? 1 : 0
+}
 
 resource "oci_identity_policy" "oke_tenancy_policies" {
   name           = "${lower(trimspace(var.app_name))}-oke-cluster-tenancy-policies-${random_string.deploy_id.result}"
@@ -39,18 +51,21 @@ resource "oci_identity_policy" "oke_tenancy_policies" {
 }
 
 locals {
+  oke_tenancy_statements = concat(
+    local.oci_grafana_metrics_statements
+  )
   oke_compartment_statements = concat(
     local.oci_grafana_logs_statements,
     var.use_encryption_from_oci_vault ? local.allow_oke_use_oci_vault_keys_statements : []
   )
-  oke_tenancy_statements = concat(
-    local.oci_grafana_metrics_statements
+  kms_compartment_statements = concat(
+    local.allow_group_manage_vault_keys_statements
   )
 }
 
 locals {
-  oke_nodes_dg = var.create_dynamic_group_for_nodes_in_compartment ? oci_identity_dynamic_group.oke_nodes_dg.0.name : "void"
-  oci_vault_key_id = var.use_encryption_from_oci_vault ? (var.create_new_encryption_key ? oci_kms_key.mushop_key[0].id : var.encryption_key_id) : null
+  oke_nodes_dg     = var.create_dynamic_group_for_nodes_in_compartment ? oci_identity_dynamic_group.oke_nodes_dg.0.name : "void"
+  oci_vault_key_id = var.use_encryption_from_oci_vault ? (var.create_new_encryption_key ? oci_kms_key.mushop_key[0].id : var.existent_encryption_key_id) : null
   oci_grafana_metrics_statements = [
     "Allow dynamic-group ${local.oke_nodes_dg} to read metrics in tenancy",
     "Allow dynamic-group ${local.oke_nodes_dg} to read compartments in tenancy"
@@ -68,13 +83,13 @@ locals {
     "Allow dynamic-group ${local.oke_nodes_dg} to inspect compartments in compartment id ${local.oke_compartment_ocid}"
   ]
   allow_oke_use_oci_vault_keys_statements = [
-    "Allow dynamic-group ${local.oke_nodes_dg} to use keys in compartment id ${local.oke_compartment_ocid} where target.key.id = '${local.oci_vault_key_id}'",
     "Allow service oke to use vaults in compartment id ${local.oke_compartment_ocid}",
-    "Allow service oke to use keys in compartment id ${local.oke_compartment_ocid} where target.key.id = '${local.oci_vault_key_id}'"
+    "Allow service oke to use keys in compartment id ${local.oke_compartment_ocid} where target.key.id = '${local.oci_vault_key_id}'",
+    "Allow dynamic-group ${local.oke_nodes_dg} to use keys in compartment id ${local.oke_compartment_ocid} where target.key.id = '${local.oci_vault_key_id}'"
   ]
   allow_group_manage_vault_keys_statements = [
     "Allow group ${var.user_admin_group_for_vault_policy} to manage vaults in compartment id ${local.oke_compartment_ocid}",
     "Allow group ${var.user_admin_group_for_vault_policy} to manage keys in compartment id ${local.oke_compartment_ocid}",
-    "Allow group ${var.user_admin_group_for_vault_policy} to use key-delegate in compartment id ${local.oke_compartment_ocid} where target.key.id = '${local.oci_vault_key_id}'"
+    "Allow group ${var.user_admin_group_for_vault_policy} to use key-delegate in compartment id ${local.oke_compartment_ocid}"
   ]
 }
