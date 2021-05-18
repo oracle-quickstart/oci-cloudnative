bebfdc31c0bec8f6493494657ee941bfcd56010b
Adao Junior <junior@users.noreply.github.com>
2021-05-18 12:38:22 -0500

    TF OCI provider bump to >= 4.26

--- deploy/complete/terraform/providers.tf
+++ deploy/complete/terraform/providers.tf
@@ -7,7 +7,7 @@ terraform {
   required_providers {
     oci = {
       source  = "hashicorp/oci"
-      version = ">= 4.23.0"
+      version = ">= 4.26.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
