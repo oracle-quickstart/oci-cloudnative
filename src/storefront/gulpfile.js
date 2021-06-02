b4baf59b5b0ffe7d562d89d39bed6cb39240050e
Adao Junior <junior@users.noreply.github.com>
2021-06-02 14:42:26 -0500

    storefront: hero images size reduction

--- src/storefront/gulpfile.js
+++ src/storefront/gulpfile.js
@@ -102,9 +102,9 @@ gulp.task('scripts', function() {
 gulp.task('images', function(done) {
   return gulp.src('src/images/**/*')
     .pipe(cache(imagemin([
-      pngquant(),
+      pngquant({quality: [0.3, 0.5]}),
       imagemin.gifsicle({interlaced: true}),
-      imagemin.mozjpeg({progressive: true, quality: 85}),
+      imagemin.mozjpeg({progressive: true, quality: 55}),
       imagemin.svgo({
         plugins: [
           { removeViewBox: true },
