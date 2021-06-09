9462fbd53e84a8e471bcddc01e8159de1a73881c
Adao Junior <junior@users.noreply.github.com>
2021-06-09 12:46:13 -0500

    variables and schema for OCI service user support

--- deploy/complete/terraform/mushop-variables.tf
+++ deploy/complete/terraform/mushop-variables.tf
@@ -46,6 +46,10 @@ variable "mushop_mock_mode_all" {
   default     = false
   description = "If enabled, will not provision ATP Database, Object Storage, or Streaming. The services will run in mock mode and will connect to an in-memory database, and the data will not persist"
 }
+variable "create_oci_service_user" {
+  default     = true
+  description = "Creates OCI Service User. Service user is needed for Email Delivery (Newsletter feature) and Stream services."
+}
 variable "newsletter_subscription_enabled" {
   default     = true
   description = "Enables newsletter subscription feature. Deploys API Gateway, Newsletter Function and uses Email Sender service"
