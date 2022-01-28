5d373981ba3d6912a5f10e071ad5eb94b14217c0
Adao Junior <junior@users.noreply.github.com>
2022-01-28 15:50:13 -0600

    storefront: extras support - 1st ODA

---
+++ src/storefront/nginx/15-storefront-extras.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+set -e
+
+ME=$(basename $0)
+
+# WWW_DIR=/usr/share/nginx/html
+WWW_DIR=.
+
+process_oda() {
+    # local ODA_SCRIPTS_DIR=/usr/share/nginx/html/scripts/oda
+    local ODA_SCRIPTS_DIR=.
+    if [[ "${ODA_ENABLED}" = true ]]; then
+        echo "$ME: Running sed to update index.html to enable ODA"
+        sed -i '' -e 's|<!-- head placeholder 1 -->|<script src="scripts/oda/settings.js"></script>|g' ${WWW_DIR}/index.html
+        sed -i '' -e 's|<!-- head placeholder 2 -->|<script src="scripts/oda/web-sdk.js" onload="initSdk('$(echo -e "\x27")'Bots'$(echo -e "\x27")')"></script>|g' ${WWW_DIR}/index.html
+
+        echo "$ME: Running envsubst to update ODA settings.js"
+        odasettingsfile="${ODA_SCRIPTS_DIR}/settings.js"
+        tmpfile=$(mktemp)
+        cp -a $odasettingsfile $tmpfile
+        cat $odasettingsfile | envsubst > $tmpfile && mv $tmpfile $odasettingsfile
+    fi
+}
+
+process_oda
+
+exit 0
