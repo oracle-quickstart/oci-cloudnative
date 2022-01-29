2807c2495da06cecb2580da96b29a83b584bf0ec
Adao Junior <junior@users.noreply.github.com>
2022-01-29 17:07:04 -0600

    storefront extras script update

--- src/storefront/nginx/15-storefront-extras.sh
+++ src/storefront/nginx/15-storefront-extras.sh
@@ -10,12 +10,16 @@ process_oda() {
     local ODA_SCRIPTS_DIR=/usr/share/nginx/html/scripts/oda
 
     if [[ "${ODA_ENABLED}" = true ]]; then
-        echo "$ME: Running sed to update index.html to enable ODA"
-        sed -i '' -e 's|<!-- head placeholder 1 -->|<script src="scripts/oda/settings.js"></script>|g' ${WWW_DIR}/index.html
-        sed -i '' -e 's|<!-- head placeholder 2 -->|<script src="scripts/oda/web-sdk.js" onload="initSdk('$(echo -e "\x27")'Bots'$(echo -e "\x27")')"></script>|g' ${WWW_DIR}/index.html
 
-        echo "$ME: Running envsubst to update ODA settings.js"
+        echo "$ME: Preparing index.html to enable Oracle Digital Assistant"
+        storefrontindex="${WWW_DIR}/index.html"
+        [ -w $storefrontindex ] && echo "$ME: Enabling ODA SDK" || echo "$ME: index.html Not Writable. Exiting..."
+        sed -i -e 's|<!-- head placeholder 1 -->|<script src="scripts/oda/settings.js"></script>|g' "$storefrontindex"
+        sed -i -e 's|<!-- head placeholder 2 -->|<script src="scripts/oda/web-sdk.js" onload="initSdk('$(echo -e "\x27")'Bots'$(echo -e "\x27")')"></script>|g' "$storefrontindex"
+
+        echo "$ME: Setting ODA variables"
         odasettingsfile="${ODA_SCRIPTS_DIR}/settings.js"
+        [ -w $odasettingsfile ] && echo "$ME: Running envsubst to update ODA settings.js" || echo "$ME: settings.js Not Writable. Exiting..."
         tmpfile=$(mktemp)
         cp -a $odasettingsfile $tmpfile
         cat $odasettingsfile | envsubst > $tmpfile && mv $tmpfile $odasettingsfile
