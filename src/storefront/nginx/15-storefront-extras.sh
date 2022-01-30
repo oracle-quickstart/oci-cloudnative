4f6882b366b59f8cc90163f9730b11506c04eab0
Adao Junior <junior@users.noreply.github.com>
2022-01-30 12:31:17 -0600

    storefront oda enable fails does not affect run

--- src/storefront/nginx/15-storefront-extras.sh
+++ src/storefront/nginx/15-storefront-extras.sh
@@ -13,16 +13,16 @@ process_oda() {
 
         echo "$ME: Preparing index.html to enable Oracle Digital Assistant"
         storefrontindex="${WWW_DIR}/index.html"
-        [ -w $storefrontindex ] && echo "$ME: Enabling ODA SDK" || echo "$ME: index.html Not Writable. Exiting..."
-        sed -i -e 's|<!-- head placeholder 1 -->|<script src="scripts/oda/settings.js"></script>|g' "$storefrontindex"
-        sed -i -e 's|<!-- head placeholder 2 -->|<script src="scripts/oda/web-sdk.js" onload="initSdk('$(echo -e "\x27")'Bots'$(echo -e "\x27")')"></script>|g' "$storefrontindex"
+        [ -w ${WWW_DIR} ] && echo "$ME: Enabling ODA SDK..." || (echo "$ME: File System Not Writable. Exiting..." && exit 0)
+        sed -i -e 's|<!-- head placeholder 1 -->|<script src="scripts/oda/settings.js"></script>|g' "$storefrontindex" || (echo "$ME: *** Failed to enable ODA SDK. Exiting..." && exit 0)
+        sed -i -e 's|<!-- head placeholder 2 -->|<script src="scripts/oda/web-sdk.js" onload="initSdk('$(echo -e "\x27")'Bots'$(echo -e "\x27")')"></script>|g' "$storefrontindex" || (echo "$ME: *** Failed to enable ODA SDK. Exiting..." && exit 0)
 
         echo "$ME: Setting ODA variables"
         odasettingsfile="${ODA_SCRIPTS_DIR}/settings.js"
-        [ -w $odasettingsfile ] && echo "$ME: Running envsubst to update ODA settings.js" || echo "$ME: settings.js Not Writable. Exiting..."
-        tmpfile=$(mktemp)
-        cp -a $odasettingsfile $tmpfile
-        cat $odasettingsfile | envsubst > $tmpfile && mv $tmpfile $odasettingsfile
+        [ -w $odasettingsfile ] && echo "$ME: Running envsubst to update ODA settings.js" || (echo "$ME: settings.js Not Writable. Exiting..." && exit 0)
+        (tmpfile=$(mktemp) && \
+        (cp -a $odasettingsfile $tmpfile) && \
+        (cat $odasettingsfile | envsubst > $tmpfile && mv $tmpfile $odasettingsfile)) || (echo "$ME: *** Failed to update settings.js. Exiting..." && exit 0)
     fi
 }
 
