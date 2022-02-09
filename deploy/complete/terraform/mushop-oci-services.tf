5f2e2596d8afba546843cc88034a38a07882b71d
Adao Junior <junior@users.noreply.github.com>
2022-02-08 23:26:24 -0600

    tf: mushop ds

--- deploy/complete/terraform/mushop-oci-services.tf
+++ deploy/complete/terraform/mushop-oci-services.tf
@@ -219,43 +219,6 @@ resource "kubernetes_secret" "oos_bucket" {
 }
 
 ##**************************************************************************
-##                            OCI KMS Vault
-##**************************************************************************
-
-### OCI Vault vault
-resource "oci_kms_vault" "mushop_vault" {
-  compartment_id = local.oke_compartment_ocid
-  display_name   = "${local.vault_display_name} - ${random_string.deploy_id.result}"
-  vault_type     = local.vault_type[0]
-
-  depends_on = [oci_identity_policy.kms_user_group_compartment_policies]
-
-  count = var.use_encryption_from_oci_vault ? (var.create_new_encryption_key ? 1 : 0) : 0
-}
-### OCI Vault key
-resource "oci_kms_key" "mushop_key" {
-  compartment_id      = local.oke_compartment_ocid
-  display_name        = "${local.vault_key_display_name} - ${random_string.deploy_id.result}"
-  management_endpoint = oci_kms_vault.mushop_vault[0].management_endpoint
-
-  key_shape {
-    algorithm = local.vault_key_key_shape_algorithm
-    length    = local.vault_key_key_shape_length
-  }
-
-  count = var.use_encryption_from_oci_vault ? (var.create_new_encryption_key ? 1 : 0) : 0
-}
-
-### Vault and Key definitions
-locals {
-  vault_display_name            = "MuShop Vault"
-  vault_key_display_name        = "MuShop Key"
-  vault_key_key_shape_algorithm = "AES"
-  vault_key_key_shape_length    = 32
-  vault_type                    = ["DEFAULT", "VIRTUAL_PRIVATE"]
-}
-
-##**************************************************************************
 ##                        OCI Service User
 ##**************************************************************************
 
@@ -415,4 +378,3 @@ resource "oci_apigateway_deployment" "newsletter_subscription" {
 
   count = var.create_new_oke_cluster ? (var.newsletter_subscription_enabled ? 1 : 0) : 0
 }
-
