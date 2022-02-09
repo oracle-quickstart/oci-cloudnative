236761f2e8adcfea7bd98cf1f2b2296fff87e672
Adao Junior <junior@users.noreply.github.com>
2022-02-08 23:21:37 -0600

    basic: output alternative hostname

--- deploy/basic/terraform/outputs.tf
+++ deploy/basic/terraform/outputs.tf
@@ -1,9 +1,12 @@
-# Copyright (c) 2019-2021 Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2019-2022 Oracle and/or its affiliates. All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
 # 
 
 output "lb_public_url" {
   value = format("http://%s", lookup(oci_load_balancer_load_balancer.mushop_lb.ip_address_details[0], "ip_address"))
+}
+output "lb_nip_host_url" {
+  value = format("http://mushop-%s.nip.io", join("", formatlist("%02x", split(".", lookup(oci_load_balancer_load_balancer.mushop_lb.ip_address_details[0], "ip_address")))))
 }
 output "autonomous_database_password" {
   value     = random_string.autonomous_database_admin_password.result
